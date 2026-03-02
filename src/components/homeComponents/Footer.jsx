def227db07dc639509908572e44f5774f346712c
Pavan Kumar <pavan.arikatla@trevistainnovations.com>
2026-03-02 20:24:57 +0530

    Fixing the footer, navigation and making app feel responsive

--- src/components/homeComponents/Footer.jsx
+++ src/components/homeComponents/Footer.jsx
@@ -4,65 +4,61 @@ function Footer() {
   return (
     <footer style={{backgroundColor: '#cfcdc6'}}>
       <div className={styles.footer}>
-      <div>
+      <div className={styles.contactBlock}>
         <img src="logo.svg" alt="Eastside" />
-        <br></br>
-        <br></br>
         <p>1418 River Drive, Suite 35 Cottonhall, CA 9622</p>
         <p>United States</p>
-        <br></br>
         <p>sale@uomo.com</p>
         <p>+1 246-345-0695</p>
-        <br></br>
+        
         <div className={styles.span}>
           <a href="https://www.facebook.com">
-          <i className="fa-brands fa-facebook"></i>
-        </a>
-        <a href="https://www.instagram.com">
-          <i className="fa-brands fa-instagram"></i>
-        </a>
-        <a href="https://www.twitter.com">
-          <i className="fa-brands fa-x-twitter"></i>
-        </a>
-        <a href="https://www.youtube.com/">
-          <i className="fa-brands fa-youtube"></i>
-        </a>
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="https://www.twitter.com">
+            <i className="fa-brands fa-x-twitter"></i>
+          </a>
+          <a href="https://www.youtube.com/">
+            <i className="fa-brands fa-youtube"></i>
+          </a>
         </div>
       </div>
 
-      <div>
-        <p><b>COMPANY</b></p><br></br>
-        <p>About Us</p><br></br>
-        <p>Career</p><br></br>
-        <p>Affiliates</p><br></br>
-        <p>Blog</p><br></br>
+      <div className={styles.companyBlock}>
+        <p><b>COMPANY</b></p>
+        <p>About Us</p>
+        <p>Career</p>
+        <p>Affiliates</p>
+        <p>Blog</p>
         <p>Contact Us</p>
       </div>
 
-      <div>
-        <p><b>SHOP</b></p><br></br>
-        <p>New Arrivals</p><br></br>
-        <p>Accessories</p><br></br>
-        <p>Men</p><br></br>
-        <p>Women</p><br></br>
+      <div className={styles.shopBlock}>
+        <p><b>SHOP</b></p>
+        <p>New Arrivals</p>
+        <p>Accessories</p>
+        <p>Men</p>
+        <p>Women</p>
         <p>Shop All</p>
       </div>
 
-      <div>
-        <p><b>HELP</b></p><br></br>
-        <p>Customer Service</p><br></br>
-        <p>My Account</p><br></br>
-        <p>Find a Store</p><br></br>
-        <p>Legal & Privacy</p><br></br>
-        <p>Contact</p><br></br>
+      <div className={styles.helpBlock}>
+        <p><b>HELP</b></p>
+        <p>Customer Service</p>
+        <p>My Account</p>
+        <p>Find a Store</p>
+        <p>Legal & Privacy</p>
+        <p>Contact</p>
+        <p>Gift Card</p>
       </div>
 
-      <div>
+      <div className={styles.subscribeBlock}>
         <p><b>SUBSCRIBE</b></p>
-        <br></br>
         <p>Be the first to get latest news about trends,</p>
         <p>promotions and more!</p>
-        <br></br>
         <form className="flex items-center justify-around gap-2 bg-white h-14 w-80">
           <input
             type="text"
@@ -74,15 +70,15 @@ function Footer() {
           </button>
         </form>
 
-        <br></br>
-        <br></br>
-        <p>Secure Payments</p><br></br>
-        <div className="flex align-center gap-10">
-          <p><img src="/mastercard.png" alt="Mastercard" className="w-10 h-6" /></p>
-          <p><img src="/visa.png" alt="Visa" className="w-10 h-6" /></p>
-          <p><img src="/paypal.png" alt="PayPal" className="w-10 h-10" /></p>
-          <p><img src="/mastercard.png" alt="Mastercard" className="w-10 h-6" /></p>
-          <p><img src="/paypal.png" alt="PayPal" className="w-10 h-10" /></p>
+        
+        
+        <p>Secure Payments</p>
+        <div className={`flex align-center gap-10 ${styles.paymentRow}`}>
+          <span className={styles.discover}>DISCOVER</span>
+          <img src="/mastercard.png" alt="Mastercard" className="w-10 h-6" />
+          <img src="/paypal.png" alt="PayPal" className="w-10 h-10" />
+          <span className={styles.skrill}>Skrill</span>
+          <img src="/visa.png" alt="Visa" className="w-10 h-6" />
         </div>
       </div>
       </div>
--- src/reusedComponents/AppNavigation.jsx
+++ src/reusedComponents/AppNavigation.jsx
@@ -1,38 +1,183 @@
 import { NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { StylesContext } from "../contexts/StylesContext";
 import styles from "./AppNavigation.module.css";
 
 export default function AppNavigation() {
   const { count, isSearchOpened, handleSearch, handleLogin, handleCart } = useContext(StylesContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isHomeDropdownOpen, setIsHomeDropdownOpen] = useState(false);
+  const [isShopDropdownOpen, setIsShopDropdownOpen] = useState(false);
+  const navRef = useRef(null);
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+    setIsHomeDropdownOpen(false);
+    setIsShopDropdownOpen(false);
+  }
+
+  useEffect(() => {
+    function handleOutsideClick(event) {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setIsHomeDropdownOpen(false);
+        setIsShopDropdownOpen(false);
+      }
+    }
+
+    function handleEsc(event) {
+      if (event.key === "Escape") {
+        setIsHomeDropdownOpen(false);
+        setIsShopDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    document.addEventListener("keydown", handleEsc);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 930) {
+        setIsHomeDropdownOpen(false);
+        setIsShopDropdownOpen(false);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <div className={styles.nav}>
-      <div className="flex items-center gap-6">
-      <NavLink to="/">
-        <img src="logo.svg" alt="UOMO"  />
-      </NavLink>
-      <p className={styles.type}>
-        <NavLink to="/men">HOME</NavLink>
-      </p>
-      <p className={styles.type}>
-        <NavLink to="/women">SHOP</NavLink>
-      </p>
-      <p className={styles.type}>
-        <NavLink to="/kids">COLLECTION</NavLink>
-      </p>
-      <p className={styles.type}>
-        <NavLink to="/beauty">JOURNAL</NavLink>
-      </p>
-      <p className={styles.type}>
-        <NavLink to="/more">LOOKBOOK</NavLink>
-      </p>
-      <p className={styles.type}>
-        <NavLink to="/pages">PAGES</NavLink>
-      </p>
+    <div ref={navRef} className={`${styles.nav} ${isMenuOpen ? styles.menuOpen : ""}`}>
+      <div className={styles.navLeft}>
+        <NavLink to="/" onClick={closeMenu}>
+          <img src="logo.svg" alt="UOMO"  />
+        </NavLink>
+        <div className={styles.navLinks}>
+          <div className={styles.homeItem}>
+            <button
+              type="button"
+              className={`${styles.type} ${styles.homeTrigger}`}
+              onClick={() => {
+                setIsHomeDropdownOpen((open) => !open);
+                setIsShopDropdownOpen(false);
+              }}
+            >
+              HOME
+            </button>
+
+            <div className={`${styles.homeDropdown} ${isHomeDropdownOpen ? styles.homeDropdownOpen : ""}`}>
+              <div className={styles.dropdownColumn}>
+                <NavLink to="/men" className={styles.dropdownHeading}>SHOP PAGES</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Default</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Topbar</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Collpase</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Simple</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Masonry</NavLink>
+                <NavLink to="/men" className={styles.dropdownHeading}>PRODUCT PAGES</NavLink>
+                <NavLink to="/item" className={styles.dropdownLink}>Default</NavLink>
+                <NavLink to="/item" className={styles.dropdownLink}>Images Left</NavLink>
+                <NavLink to="/item" className={styles.dropdownLink}>Image Grid</NavLink>
+                <NavLink to="/item" className={styles.dropdownLink}>Image Slider</NavLink>
+                <NavLink to="/item" className={styles.dropdownLink}>Images Stacked</NavLink>
+              </div>
+
+              <div className={styles.dropdownColumn}>
+                <NavLink to="/men" className={styles.dropdownHeading}>OTHER PAGES</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Collection</NavLink>
+                <NavLink to="/more" className={styles.dropdownLink}>LookBook</NavLink>
+                <NavLink to="/fashion" className={styles.dropdownLink}>Categories Page</NavLink>
+                <NavLink to="/cart" className={styles.dropdownLink}>Shopping Cart</NavLink>
+                <NavLink to="/wishlist" className={styles.dropdownLink}>Wishlist</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Order Tracking</NavLink>
+                <NavLink to="/cart" className={styles.dropdownLink}>Checkout</NavLink>
+                <NavLink to="/cart" className={styles.dropdownLink}>Checkout - 2 Columns</NavLink>
+              </div>
+
+              <div className={styles.dropdownColumn}>
+                <NavLink to="/men" className={styles.dropdownHeading}>ELEMENTS</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Accordion</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Pricing Table</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Google Maps</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Message Box</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Progress Bars</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Charts</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Icon Box</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Product Tabs</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Products Grid</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Tabs</NavLink>
+                <NavLink to="/men" className={styles.dropdownLink}>Video Players</NavLink>
+              </div>
+
+              <div className={styles.dropdownPromo}>
+                <strong>NEW HORIZONS</strong>
+                <button type="button">SHOP NOW</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.shopItem}>
+            <button
+              type="button"
+              className={`${styles.type} ${styles.shopTrigger}`}
+              onClick={() => {
+                setIsShopDropdownOpen((open) => !open);
+                setIsHomeDropdownOpen(false);
+              }}
+            >
+              SHOP
+            </button>
+
+            <div className={`${styles.shopDropdown} ${isShopDropdownOpen ? styles.shopDropdownOpen : ""}`}>
+              <div className={styles.dropdownColumn}>
+                <NavLink to="/women" className={styles.dropdownHeading}>BLOG STYLES</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Alternative</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Small images</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Blog chess</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Masonry grid</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Infinit scrollingFEATURE</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>With background</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Blog flat</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Default flat</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Blog mask</NavLink>
+              </div>
+
+              <div className={styles.dropdownColumn}>
+                <NavLink to="/women" className={styles.dropdownHeading}>SINGLE POST</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Standard Post</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Image Post</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Video Post</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Audio Post</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Gallery Post</NavLink>
+              </div>
+
+              <div className={styles.dropdownColumn}>
+                <NavLink to="/women" className={styles.dropdownHeading}>NAVIGATION</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Simple</NavLink>
+                <NavLink to="/women" className={styles.dropdownLink}>Image Background</NavLink>
+              </div>
+            </div>
+          </div>
+          <p className={styles.type}>
+            <NavLink to="/kids" onClick={closeMenu}>COLLECTION</NavLink>
+          </p>
+          <p className={styles.type}>
+            <NavLink to="/beauty" onClick={closeMenu}>JOURNAL</NavLink>
+          </p>
+          <p className={styles.type}>
+            <NavLink to="/more" onClick={closeMenu}>LOOKBOOK</NavLink>
+          </p>
+          <p className={styles.type}>
+            <NavLink to="/men" onClick={closeMenu}>PAGES</NavLink>
+          </p>
+        </div>
       </div>
 
-      <div className="flex gap-8">
+      <div className={styles.navRight}>
         <button onClick={handleSearch}>
           <li>
             {!isSearchOpened ? <i className="fa-brands fa-sistrix cursor-pointer"></i> : <p className="cursor-pointer">&#x1D5B7;</p>}
@@ -57,7 +202,14 @@ export default function AppNavigation() {
           </li>
         </button>
 
-        <NavLink to=""><i className="fa-solid fa-bars"></i></NavLink>
+        <button
+          type="button"
+          className={styles.menuToggle}
+          onClick={() => setIsMenuOpen((open) => !open)}
+          aria-label="Toggle navigation menu"
+        >
+          <i className="fa-solid fa-bars"></i>
+        </button>
       </div>
 
     </div>
